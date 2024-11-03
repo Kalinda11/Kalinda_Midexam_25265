5e48ecbd767cc9bf69065cf1cf37d45ff23517f2
Kalinda11 <kalindaben2021@gmail.com>
2024-11-03 18:21:53 +0200

    Add files via upload

---
+++ HospitalManagementApp/src/main/java/rw/wetech/HospitalManagementSystem/userRepository/UserRepository.java
@@ -0,0 +1,14 @@
+package rw.wetech.HospitalManagementSystem.userRepository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import rw.wetech.HospitalManagementSystem.model.User;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+    User findByEmail(String email);
+    List<User> findByUsernameContainingOrEmailContaining(String username, String email);
+}
